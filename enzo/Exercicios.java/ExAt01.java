898104c52e99a1bc17ae579f3192a300abdbfdcb
EnzzoBez <151688241+EnzzoBez@users.noreply.github.com>
2024-02-27 23:56:27 +0000

    Somarnumeros

---
+++ enzo/Exercicios.java/ExAt01.java
@@ -0,0 +1,11 @@
+public class ExAt01{
+    public static void main (String[] args){
+        double soma = 0;
+        for(int  i=0; i<args.length; i++){
+            soma += Double.parseDouble(args[i]);
+        
+        } 
+        System.out.println (soma);
+    } 
+}
+
