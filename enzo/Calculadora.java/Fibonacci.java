2304a5c593b54de605169e4424fc75207443412a
EnzzoBez <151688241+EnzzoBez@users.noreply.github.com>
2024-02-29 23:03:34 +0000

    Fibonacci

---
+++ enzo/Calculadora.java/Fibonacci.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Digite o número até o qual deseja gerar a série de Fibonacci:");
+        int limite = scanner.nextInt();
+        
+        System.out.println("Série de Fibonacci até " + limite + ":");
+        for (int i = 0; fibonacci(i) <= limite; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+        
+        scanner.close();
+    }
+    
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+}
