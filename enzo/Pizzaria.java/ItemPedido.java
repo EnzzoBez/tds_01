11818271ef8ffdf2c9b79c061727e47ee9b0f219
Enzzo Bez <151688241+EnzzoBez@users.noreply.github.com>
2024-03-14 22:03:49 +0000

    Item pedido

---
+++ enzo/Pizzaria.java/ItemPedido.java
@@ -0,0 +1,18 @@
+public class ItemDoPedido {
+    private String sabor;
+    private double valor;
+    private String tipo;
+
+    public void setTipo(String tipo){
+        this.tipo = tipo;
+    }
+    public void setSabor(String sabor){
+        this.sabor = sabor;
+    }
+    public void setValor(double valor){
+        this.valor = valor;
+    }
+    public double getValor(){
+        return this.valor;
+    }
+}
